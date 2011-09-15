6685ca075ef00a73fbe6233309f262986171b17d
Steven <scjackson@linux.com>
2011-09-15 17:04:40 -0400

    more R data with pdf generation

--- cardToTab.pl
+++ cardToTab.pl
@@ -22,10 +22,13 @@ print FINAL "seq_num\tparent_seq_num\tmax_status\t\n";
 
 print R1 'si <- read.csv("' . $dir . '/delim_data/storm_info.tab",sep="\t",head=TRUE)
 names(si)
+pdf("' . $dir . '/r/plots.pdf")
 plot(si$storm_num_days,si$year,xlab="Number of days in Storm",ylab="Year")
 plot(si$year,si$storm_num_this_year,xlab="Year",ylab="Number of Storms")
 plot(si$sss,si$year,xlab="Severity of Storm",ylab="Year")
-
+barplot(table(si$year)) #storms per year
+barplot(table(si$month)) #storms per month
+dev.off()
 ';
 
 while($line = <INPUT>){
--- r/r1.R
+++ r/r1.R
@@ -1,5 +1,9 @@
 si <- read.csv("C:/Users/sjackson/Desktop/dev/hurricane/delim_data/storm_info.tab",sep="\t",head=TRUE)
 names(si)
+pdf("C:/Users/sjackson/Desktop/dev/hurricane/r/plots.pdf")
 plot(si$storm_num_days,si$year,xlab="Number of days in Storm",ylab="Year")
+plot(si$year,si$storm_num_this_year,xlab="Year",ylab="Number of Storms")
 plot(si$sss,si$year,xlab="Severity of Storm",ylab="Year")
-
+barplot(table(si$year)) #storms per year
+barplot(table(si$month)) #storms per month
+dev.off()
